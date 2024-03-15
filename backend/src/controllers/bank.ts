78d522f60331edf6411ea93d711823920f7547fb
HamaadYousaf <hamaaadyousaf@outlook.com>
2024-03-15 18:40:17 -0400

    transfer money

--- backend/src/controllers/bank.ts
+++ backend/src/controllers/bank.ts
@@ -22,7 +22,91 @@ export const getBalance: RequestHandler = async (req, res, next) => {
     }
 };
 
-export const transfer = () => {};
+interface TransferBody {
+    userIdSend: string;
+    userIdReceive: string;
+    roomId: string;
+    amount: number;
+}
+export const transfer: RequestHandler<
+    unknown,
+    unknown,
+    TransferBody,
+    unknown
+> = async (req, res, next) => {
+    const userIdSend = req.body.userIdSend;
+    const userIdReceive = req.body.userIdReceive;
+    const roomId = req.body.roomId;
+    const amount = req.body.amount;
+
+    try {
+        if (!userIdSend || !userIdReceive || !roomId || !amount) {
+            throw createHttpError(400, "Missing parameters");
+        }
+
+        if (userIdSend === userIdReceive) {
+            throw createHttpError(
+                400,
+                "User cannot be both sender and reciever"
+            );
+        }
+
+        const userBankSend = await prismaInstance.bank.findFirst({
+            where: {
+                userId: userIdSend,
+            },
+        });
+        const userBankReceive = await prismaInstance.bank.findFirst({
+            where: {
+                userId: userIdReceive,
+            },
+        });
+
+        if (
+            userBankSend?.roomId !== roomId ||
+            userBankReceive?.roomId !== roomId ||
+            !userBankSend ||
+            !userBankReceive
+        ) {
+            throw createHttpError(409, "Users is not in room");
+        }
+
+        const newBalanceSend = userBankSend.balance - amount;
+
+        if (newBalanceSend < 0) {
+            throw createHttpError(400, "Not enough funds");
+        }
+
+        const newBalanceRecieve = userBankReceive.balance + amount;
+
+        const newUserBankSend = await prismaInstance.bank.update({
+            where: {
+                userId: userIdSend,
+            },
+            data: {
+                balance: newBalanceSend,
+            },
+        });
+
+        const newUserBankReceive = await prismaInstance.bank.update({
+            where: {
+                userId: userIdReceive,
+            },
+            data: {
+                balance: newBalanceRecieve,
+            },
+        });
+
+        res.status(201).send({
+            data: {
+                sender: newUserBankSend,
+                reciever: newUserBankReceive,
+            },
+        });
+    } catch (error) {
+        next(error);
+    }
+};
 
 export const claimFreeParking = () => {};
 
